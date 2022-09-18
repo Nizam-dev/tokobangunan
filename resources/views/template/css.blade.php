25bedc628bb1942f966e99d66b685072686e2f36
Nizam <mahfudzkhoirunnizam@gmail.com>
2022-09-18 18:01:25 +0700

    template first

---
+++ resources/views/template/css.blade.php
@@ -0,0 +1,14 @@
+<link rel="stylesheet" href="{{asset('public/template/assets/vendors/mdi/css/materialdesignicons.min.css')}}">
+<link rel="stylesheet" href="{{asset('public/template/assets/vendors/flag-icon-css/css/flag-icon.min.css')}}">
+<link rel="stylesheet" href="{{asset('public/template/assets/vendors/css/vendor.bundle.base.css')}}">
+<!-- endinject -->
+<!-- Plugin css for this page -->
+<link rel="stylesheet" href="{{asset('public/template/assets/vendors/font-awesome/css/font-awesome.min.css')}}" />
+<link rel="stylesheet" href="{{asset('public/template/assets/vendors/bootstrap-datepicker/bootstrap-datepicker.min.css')}}">
+<!-- End plugin css for this page -->
+<!-- inject:css -->
+<!-- endinject -->
+<!-- Layout styles -->
+<link rel="stylesheet" href="{{asset('public/template/assets/css/style.css')}}">
+<!-- End layout styles -->
+<link rel="shortcut icon" href="{{asset('public/template/assets/images/favicon.png')}}" />
---
+++ resources/views/template/js.blade.php
@@ -0,0 +1,15 @@
+<!-- plugins:js -->
+<script src="{{asset('public/template/assets/vendors/js/vendor.bundle.base.js')}}"></script>
+<!-- endinject -->
+<!-- Plugin js for this page -->
+<script src="{{asset('public/template/assets/vendors/chart.js/Chart.min.js')}}"></script>
+<script src="{{asset('public/template/assets/vendors/jquery-circle-progress/js/circle-progress.min.js')}}"></script>
+<!-- End plugin js for this page -->
+<!-- inject:js -->
+<script src="{{asset('public/template/assets/js/off-canvas.js')}}"></script>
+<script src="{{asset('public/template/assets/js/hoverable-collapse.js')}}"></script>
+<script src="{{asset('public/template/assets/js/misc.js')}}"></script>
+<!-- endinject -->
+<!-- Custom js for this page -->
+<script src="{{asset('public/template/assets/js/dashboard.js')}}"></script>
+<!-- End custom js for this page -->
